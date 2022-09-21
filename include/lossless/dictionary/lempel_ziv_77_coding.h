562439f96e6ab4a522dc90d82c0ce9c684b66c6e
Jonas Haubold <jonas.j.haubold@daimler.com>
2022-09-21 18:42:18 +0200

    Implemented LZ77 coding

--- include/lossless/dictionary/lempel_ziv_77_coding.h
+++ include/lossless/dictionary/lempel_ziv_77_coding.h
@@ -10,17 +10,75 @@ namespace cl::lossless::dictionary
 class LempelZiv77Coding
 {
 public:
-  [[nodiscard]] const std::pair<std::vector<char>, std::map<char, std::string>> encode(const std::vector<char>& symbols) const
+  [[nodiscard]] const std::vector<std::tuple<std::size_t, std::size_t, char>> encode(const std::vector<char>& symbols) const
   {
-    std::vector<char> result;
-    std::map<char, std::string> symbol_replace_table;
+    std::vector<std::tuple<std::size_t, std::size_t, char>> triplets;
     if (symbols.empty())
-      return std::make_pair(result, symbol_replace_table);
-    result.insert(result.end(), symbols.begin(), symbols.end());
+      return triplets;
 
-    // TODO
+    constexpr unsigned int SIZE_LOOK_AHEAD_BUFFER = 4;
+    constexpr unsigned int SIZE_SEARCH_BUFFER = 6;
+    
+    bool end_reached = false;
 
-    return std::make_pair(result, symbol_replace_table);
+    auto index_is_valid = [&symbols](auto i){
+      return i < symbols.size();
+    };
+
+    for(std::size_t i = 0; i < symbols.size();)
+    {
+      std::string current = "";
+      for(auto j = 0; j != SIZE_LOOK_AHEAD_BUFFER; ++j)
+      {
+        if(!index_is_valid(i+j))
+        {
+          end_reached = true;
+          break;
+        }
+        current.push_back(symbols[i+j]);
+      }  
+
+      std::string current_search_buffer;
+      for(auto j = 0; j != SIZE_SEARCH_BUFFER; ++j)
+        if(index_is_valid(i - SIZE_SEARCH_BUFFER + j))
+          current_search_buffer.push_back(symbols[i - SIZE_SEARCH_BUFFER + j]);
+
+      std::size_t o = 0; // How many steps to go back to find the start of the matching string
+      std::size_t l = 0; // Length of the match
+      char c = ' ';
+
+      bool match_found = false;
+      for(auto j = 0; j != SIZE_LOOK_AHEAD_BUFFER - 1; ++j)
+      {
+        if(j != 0)
+          current.erase(current.size() - 1);
+        if(const auto index = current_search_buffer.rfind(current); index != std::string::npos)
+        {
+          match_found = true;
+          const auto pos = current_search_buffer.size() - index;
+          o = pos;
+          l = current.size();
+
+          if(index_is_valid(i+l))
+            c = symbols[i+l];
+          else
+            end_reached = true;
+          break;
+        }
+        else
+          continue;
+      }
+      if(!match_found)
+        c = symbols[i];
+      
+      triplets.emplace_back(o, l, c);
+      if(end_reached)
+        i = symbols.size();
+      else
+        i += l + 1;      
+    }
+
+    return triplets;
   }
 };
 } // namespace cl::lossless::dictionary
--- test/lempel_ziv_coding.cc
+++ test/lempel_ziv_coding.cc
@@ -13,25 +13,27 @@ TEST_F(LempelZiv77CodingTest, NoSymbols)
   std::vector<char> symbols = {};
   cl::lossless::dictionary::LempelZiv77Coding lz77;
   const auto code_symbols = lz77.encode(symbols);
-  EXPECT_EQ(0, code_symbols.first.size());
-  EXPECT_EQ(0, code_symbols.second.size());
+  EXPECT_EQ(0, code_symbols.size());
 }
 TEST_F(LempelZiv77CodingTest, SymbolsSet1)
 {
-  std::vector<char> symbols = {'a', 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+  std::vector<char> symbols = {'a', 'b', 'a', 'b', 'c', 'b', 'a', 'b', 'a', 'b', 'a', 'a'};
   cl::lossless::dictionary::LempelZiv77Coding lz77;
   const auto code_symbols = lz77.encode(symbols);
-  EXPECT_EQ(3, code_symbols.second.size());
-  EXPECT_EQ(5, code_symbols.first.size());
-  EXPECT_EQ('X', code_symbols.first[0]);
-  EXPECT_EQ('d', code_symbols.first[1]);
-  EXPECT_EQ('X', code_symbols.first[2]);
-  EXPECT_EQ('a', code_symbols.first[3]);
-  EXPECT_EQ('c', code_symbols.first[4]);
-  EXPECT_TRUE(code_symbols.second.find('Z') != code_symbols.second.end());
-  EXPECT_TRUE(code_symbols.second.find('Y') != code_symbols.second.end());
-  EXPECT_TRUE(code_symbols.second.find('X') != code_symbols.second.end());
-  EXPECT_EQ("aa", code_symbols.second.at('Z'));
-  EXPECT_EQ("Za", code_symbols.second.at('Y'));
-  EXPECT_EQ("Yb", code_symbols.second.at('X'));
+  EXPECT_EQ(5, code_symbols.size());
+  EXPECT_EQ(0, std::get<0>(code_symbols[0]));
+  EXPECT_EQ(0, std::get<1>(code_symbols[0]));
+  EXPECT_EQ('a', std::get<2>(code_symbols[0]));
+  EXPECT_EQ(0, std::get<0>(code_symbols[1]));
+  EXPECT_EQ(0, std::get<1>(code_symbols[1]));
+  EXPECT_EQ('b', std::get<2>(code_symbols[1]));
+  EXPECT_EQ(2, std::get<0>(code_symbols[2]));
+  EXPECT_EQ(2, std::get<1>(code_symbols[2]));
+  EXPECT_EQ('c', std::get<2>(code_symbols[2]));
+  EXPECT_EQ(4, std::get<0>(code_symbols[3]));
+  EXPECT_EQ(3, std::get<1>(code_symbols[3]));
+  EXPECT_EQ('a', std::get<2>(code_symbols[3]));
+  EXPECT_EQ(2, std::get<0>(code_symbols[4]));
+  EXPECT_EQ(2, std::get<1>(code_symbols[4]));
+  EXPECT_EQ('a', std::get<2>(code_symbols[4]));
 }
