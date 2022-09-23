a0b8937a47489fdfe989c6125fd461d4c52bf9ab
Jonas Haubold <jonas.j.haubold@daimler.com>
2022-09-23 16:18:12 +0200

    Adapt byte pair coding to be more generalized

--- include/lossless/dictionary/byte_pair_coding.h
+++ include/lossless/dictionary/byte_pair_coding.h
@@ -19,24 +19,30 @@ namespace cl::lossless::dictionary
 class BytePairCoding
 {
 public:
-/**
- * @brief Encodes a collection of given symbols using the 'Byte-Pair-Coding' algorithm.
- * 
- * @param symbols The symbols to be encoded
- * @return const std::pair<std::vector<char>, std::map<char, std::string>> The resulting encoded collection of symbols
- */
-  [[nodiscard]] const std::pair<std::vector<char>, std::map<char, std::string>> encode(const std::vector<char>& symbols) const
+  /**
+   * @brief Encodes a collection of given symbols using the 'Byte-Pair-Coding' algorithm.
+   *
+   * @param symbols The symbols to be encoded
+   * @return const std::pair<std::vector<char>, std::map<char, std::string>> The resulting encoded collection of symbols
+   */
+
+  template <typename S, typename T, typename U>
+  [[nodiscard]] const std::pair<std::vector<T>, std::map<T, std::vector<S>>> encode(const std::vector<S>& symbols, U replacement_symbol_generator) const
   {
-    std::vector<char> result;
-    std::map<char, std::string> symbol_replace_table;
+    using SymbolType = S;
+    using ReplacementSymbolType = T;
+
+    std::vector<ReplacementSymbolType>
+        result;
+    std::map<ReplacementSymbolType, std::vector<SymbolType>> symbol_replace_table;
+
     if (symbols.empty())
       return std::make_pair(result, symbol_replace_table);
     result.insert(result.end(), symbols.begin(), symbols.end());
-    std::vector<std::pair<std::string, unsigned int>> frequencies;
-
+    std::vector<std::pair<std::vector<SymbolType>, unsigned int>> frequencies;
     for (std::size_t i = 0; i < result.size() - 1; ++i)
     {
-      const std::string s{result[i], result[i + 1]};
+      const std::vector<SymbolType> s{result[i], result[i + 1]};
       const auto pos = std::find_if(frequencies.begin(), frequencies.end(), [&s](const auto& f)
                                     {
                                       return f.first == s;
@@ -46,27 +52,24 @@ public:
       else
         pos->second += 1;
     }
-
     std::sort(frequencies.begin(), frequencies.end(), [](auto& left, auto& right)
               {
                 return left.second > right.second;
               });
     if (frequencies.front().second > 1)
     {
-      static unsigned int replace_symbol_offset = 0;
+      ReplacementSymbolType replace_symbol = replacement_symbol_generator(); // Yes, we can only replace a limited amount of symbols. But good enough here...
       for (std::size_t i = 0; i < result.size() - 1; ++i)
       {
-        const std::string s{result[i], result[i + 1]};
+        const std::vector<SymbolType> s{result[i], result[i + 1]};
         if (s == frequencies.front().first)
         {
-          char replace_symbol = 'Z' - replace_symbol_offset; // Yes, we can only replace a limited amount of symbols. But good enough here...
           symbol_replace_table[replace_symbol] = s;
           result[i] = replace_symbol;
           result.erase(result.begin() + i + 1);
         }
       }
-      replace_symbol_offset += 1;
-      const std::pair<std::vector<char>, std::map<char, std::string>> recursive_result = encode(result);
+      const auto recursive_result = encode<SymbolType, ReplacementSymbolType>(result, replacement_symbol_generator);
       symbol_replace_table.insert(recursive_result.second.begin(), recursive_result.second.end());
       result = recursive_result.first;
     }
--- test/byte_pair_coding.cc
+++ test/byte_pair_coding.cc
@@ -12,7 +12,16 @@ TEST_F(BytePairCodingTest, NoSymbols)
 {
   std::vector<char> symbols = {};
   cl::lossless::dictionary::BytePairCoding bpc;
-  const auto code_symbols = bpc.encode(symbols);
+  constexpr auto symbol_replacement_generator = []()
+  {
+    static unsigned int symbol_A_offset = 0;
+    if (symbol_A_offset == 26)
+      throw std::runtime_error("Cannot generate more symbols for byte pair encoding");
+    const auto symbol = 'Z' - symbol_A_offset;
+    symbol_A_offset += 1;
+    return symbol;
+  };
+  const auto code_symbols = bpc.encode<char, char>(symbols, symbol_replacement_generator);
   EXPECT_EQ(0, code_symbols.first.size());
   EXPECT_EQ(0, code_symbols.second.size());
 }
@@ -20,7 +29,16 @@ TEST_F(BytePairCodingTest, SymbolsSet1)
 {
   std::vector<char> symbols = {'a', 'a', 'a', 'b', 'd', 'a', 'a', 'a', 'b', 'a', 'c'};
   cl::lossless::dictionary::BytePairCoding bpc;
-  const auto code_symbols = bpc.encode(symbols);
+  constexpr auto symbol_replacement_generator = []()
+  {
+    static unsigned int symbol_A_offset = 0;
+    if (symbol_A_offset == 26)
+      throw std::runtime_error("Cannot generate more symbols for byte pair encoding");
+    const auto symbol = 'Z' - symbol_A_offset;
+    symbol_A_offset += 1;
+    return symbol;
+  };
+  const auto code_symbols = bpc.encode<char, char>(symbols, symbol_replacement_generator);
   EXPECT_EQ(3, code_symbols.second.size());
   EXPECT_EQ(5, code_symbols.first.size());
   EXPECT_EQ('X', code_symbols.first[0]);
@@ -31,7 +49,10 @@ TEST_F(BytePairCodingTest, SymbolsSet1)
   EXPECT_TRUE(code_symbols.second.find('Z') != code_symbols.second.end());
   EXPECT_TRUE(code_symbols.second.find('Y') != code_symbols.second.end());
   EXPECT_TRUE(code_symbols.second.find('X') != code_symbols.second.end());
-  EXPECT_EQ("aa", code_symbols.second.at('Z'));
-  EXPECT_EQ("Za", code_symbols.second.at('Y'));
-  EXPECT_EQ("Yb", code_symbols.second.at('X'));
+  EXPECT_EQ('a', code_symbols.second.at('Z')[0]);
+  EXPECT_EQ('a', code_symbols.second.at('Z')[1]);
+  EXPECT_EQ('Z', code_symbols.second.at('Y')[0]);
+  EXPECT_EQ('a', code_symbols.second.at('Y')[1]);
+  EXPECT_EQ('Y', code_symbols.second.at('X')[0]);
+  EXPECT_EQ('b', code_symbols.second.at('X')[1]);
 }
