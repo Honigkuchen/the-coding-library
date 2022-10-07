f3ee8830d453247667999831fa58ae01f4e861fa
Jonas Haubold <jonas.j.haubold@daimler.com>
2022-10-07 20:01:49 +0200

    Add more generic DCT

--- include/lossy/transform_type/DCT1.h
+++ include/lossy/transform_type/DCT1.h
@@ -4,25 +4,46 @@
 #include <cmath>
 
 // STL includes
+#include <type_traits>
 #include <vector>
+
+// Debug includes
+#include <iostream>
 
 namespace cl::lossy::transform_type
 {
 class DCT1
 {
-public:
+private:
   template <typename T>
-  [[nodiscard]] constexpr auto transform(const std::vector<T>& numbers) const
+  [[nodiscard]] constexpr double alpha(const T& w) const
   {
-    std::vector<T> result;
+    if (w == 0)
+      return 1 / std::sqrt(2);
+    return 1.0;
+  }
 
-    for (std::size_t k = 0; k < numbers.size(); ++k)
+public:
+  template <std::size_t X, std::size_t Y, typename Predicate, typename T, typename = std::enable_if_t<std::is_default_constructible_v<T>>>
+  [[nodiscard]] constexpr auto transform(const T& numbers, Predicate p) const
+  {
+    T result = {};
+    if (numbers.empty())
+      return result;
+    for (std::size_t u = 0; u != X; ++u)
     {
-      const bool even = (k % 2) == 0;
-      auto X_k = 1 / 2 * (numbers.front() + (even ? 1 : -1) * numbers.back());
-      for (std::size_t n = 1; n != numbers.size() - 2; ++n)
-        X_k += numbers[n] * std::cos(M_PI / (numbers.size() - 1) * n * k);
-      result.push_back(X_k);
+      const auto alpha_u = alpha(u);
+      for (std::size_t v = 0; v != Y; ++v)
+      {
+        const auto alpha_v = alpha(v);
+        double G_u_v = (1.0 / 4.0) * alpha_u * alpha_v;
+        double sum = 0;
+        for (std::size_t x = 0; x != X; ++x)
+          for (std::size_t y = 0; y != Y; ++y)
+            sum += p(numbers, x, y) * std::cos((2.0 * x + 1.0) / 16.0 * u * M_PI) * std::cos((2.0 * y + 1.0) / 16.0 * v * M_PI);
+
+        p(result, u, v) = G_u_v * sum;
+      }
     }
     return result;
   }
--- test/DCT1.cc
+++ test/DCT1.cc
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 
 #include <gtest/gtest.h>
@@ -7,22 +8,52 @@
 class DCT1CodingTest : public ::testing::Test
 {
 };
+constexpr auto X = 8;
+constexpr auto Y = 8;
+const auto accessor = [](auto& m, const auto& x, const auto& y) -> auto&
+{
+  if (x >= X)
+    throw std::range_error("x index in matrix out of range");
+  if (y >= Y)
+    throw std::range_error("y index in matrix out of range");
+
+  return m[x * X + y];
+};
 TEST_F(DCT1CodingTest, NoSymbols)
 {
   std::vector<double> numbers = {};
   cl::lossy::transform_type::DCT1 dct;
-  const auto transformed_numbers = dct.transform(numbers);
+  const auto transformed_numbers = dct.transform<X, Y>(
+      numbers, accessor);
   EXPECT_EQ(0, transformed_numbers.size());
 }
 TEST_F(DCT1CodingTest, NumbersTest1)
 {
-  const std::vector<int> numbers = {12, 24, 33, 0, 15};
+  constexpr std::array<double, X* Y> m = {-76, -73, -67, -62, -58, -67, -64, -55,
+                                          -65, -69, -73, -38, -19, -43, -59, -56,
+                                          -66, -69, -60, -15, 16, -24, -62, -55,
+                                          -65, -70, -57, -6, 26, -22, -58, -59,
+                                          -61, -67, -60, -24, -2, -40, -60, -58,
+                                          -49, -63, -68, -58, -51, -60, -70, -53,
+                                          -43, -57, -64, -69, -73, -67, -63, -45,
+                                          -41, -49, -59, -60, -63, -52, -50, -34};
   cl::lossy::transform_type::DCT1 dct;
-  const auto transformed_numbers = dct.transform(numbers);
-  EXPECT_EQ(5, transformed_numbers.size());
-  EXPECT_EQ(57, transformed_numbers[0]);
-  EXPECT_EQ(16, transformed_numbers[1]);
-  EXPECT_EQ(-33, transformed_numbers[2]);
-  EXPECT_EQ(-16, transformed_numbers[3]);
-  EXPECT_EQ(9, transformed_numbers[4]);
+  const auto transformed_numbers = dct.transform<X, Y>(m, accessor);
+
+  const auto round = [](const auto& v)
+  {
+    return std::round(v * 100.0) / 100.0;
+  };
+  constexpr std::array expected_result = {-415.37, -30.19, -61.20, 27.24, 56.12, -20.10, -2.39, 0.46,
+                                          4.47, -21.86, -60.76, 10.25, 13.15, -7.09, -8.54, 4.88,
+                                          -46.83, 7.37, 77.13, -24.56, -28.91, 9.93, 5.42, -5.65,
+                                          -48.53, 12.07, 34.10, -14.76, -10.24, 6.30, 1.83, 1.95,
+                                          12.12, -6.55, -13.20, -3.95, -1.87, 1.75, -2.79, 3.14,
+                                          -7.73, 2.91, 2.38, -5.94, -2.38, 0.94, 4.30, 1.85,
+                                          -1.03, 0.18, 0.42, -2.42, -0.88, -3.02, 4.12, -0.66,
+                                          -0.17, 0.14, -1.07, -4.19, -1.17, -0.10, 0.50, 1.68};
+  EXPECT_EQ(X * Y, transformed_numbers.size());
+  EXPECT_EQ(expected_result.size(), transformed_numbers.size());
+  for (std::size_t c = 0; c < expected_result.size(); ++c)
+    EXPECT_EQ(round(transformed_numbers[c]), expected_result[c]);
 }
