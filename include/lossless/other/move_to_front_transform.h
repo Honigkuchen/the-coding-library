1e4587af682d36be6c79e94423dc23e1c4f3bc3a
Jonas Haubold <jonas.j.haubold@daimler.com>
2022-10-16 22:30:54 +0200

    Improve move to front transform

--- include/lossless/other/move_to_front_transform.h
+++ include/lossless/other/move_to_front_transform.h
@@ -3,6 +3,7 @@
 // STL includes
 #include <deque>
 #include <iterator>
+#include <vector>
 
 namespace cl::lossless::other
 {
@@ -22,15 +23,21 @@ public:
   {
     std::deque<char> dictionary_copy = dictionary;
     std::vector<unsigned int> result;
+    result.reserve(symbols.size());
+
     for (const auto& s : symbols)
     {
       const auto symbol_position = std::find(dictionary_copy.begin(), dictionary_copy.end(), s);
-      const unsigned int rank =
-          std::distance(dictionary_copy.begin(), symbol_position);
-      result.push_back(rank);
-      dictionary_copy.erase(symbol_position);
-      dictionary_copy.push_front(s);
+      if (symbol_position != dictionary_copy.end())
+      {
+        const unsigned int rank =
+            std::distance(dictionary_copy.begin(), symbol_position);
+        result.push_back(rank);
+        dictionary_copy.erase(symbol_position);
+        dictionary_copy.push_front(s);
+      }
     }
+    result.shrink_to_fit();
     return result;
   }
 };
